26a037bf68d37e055360d976fff9f0dcee88ac4b
kanikak03 <kanikakharbanda@gmail.com>
2016-12-12 11:17:31 +0530

    Added Cards - Flow is little better

--- app/src/main/java/com/example/rajat/abhyuday/Home.java
+++ app/src/main/java/com/example/rajat/abhyuday/Home.java
@@ -1,12 +1,22 @@
 package com.example.rajat.abhyuday;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 //import android.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,22 +27,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.rajat.abhyuday.dummy.DummyContent;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.rajat.abhyuday.R.*;
 
 public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, CallFragment.OnFragmentInteractionListener, RegisterFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, CallFragment.OnFragmentInteractionListener, RegisterFragment.OnFragmentInteractionListener{
+
+
+    private RecyclerView recyclerView;
+    private AlbumsAdapter adapter;
+    private List<Album> albumList;
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
+     *
      */
     private GoogleApiClient client;
+    //SupportMapFragment supportMapFragment;
+    //MapFragment mapf;
 
     @Override
     public void onFragmentInteraction(Uri uri) {
@@ -43,11 +72,37 @@ public class Home extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //supportMapFragment=SupportMapFragment.newInstance();
+        setContentView(layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        Toolbar toolbar1 = (Toolbar) findViewById(id.toolbaralbum);
+//        setSupportActionBar(toolbar);
+
+        //initCollapsingToolbar();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        albumList = new ArrayList<>();
+        adapter = new AlbumsAdapter(this, albumList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        prepareAlbums();
+
+//        try {
+//            Glide.with(this).load(R.drawable.cover).into((ImageView) findViewById(R.id.backdrop));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,22 +111,24 @@ public class Home extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, string.navigation_drawer_open, string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView) findViewById(id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -105,6 +162,8 @@ public class Home extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        android.support.v4.app.FragmentManager fm=getSupportFragmentManager();
+
         int id = item.getItemId();
 
         if (id == R.id.nav_event) {
@@ -120,14 +179,31 @@ public class Home extends AppCompatActivity
             Toast.makeText(this, "Register", Toast.LENGTH_LONG).show();
 
 
-        } else if (id == R.id.nav_conus) {
+        } else if (id == R.id.nav_findus) {
+
+//            Intent mHome=new Intent(Home.this,MapsActivity.class);
+//            Home.this.startActivity(mHome);
+//            Home.this.finish();
+//            MapFragment mapf;
+//        mapf=((MapFragment) getFragmentManager().findFragmentById(R.id.map));
+//        mapf.getMapAsync(MapsActivity.OnMapReadyCallback);
+
+       //MapsActivity ma = new MapsActivity();
+            //android.app.FragmentManager manager = getFragmentManager();
+           // manager.beginTransaction().replace(R.id.content_home, ma).commit();
+
+
+
+
+        }else if (id == R.id.nav_conus) {
 
             CallFragment Callfragment = new CallFragment();
             FragmentManager manager = getSupportFragmentManager();
             manager.beginTransaction().replace(R.id.content_home, new CallFragment()).commit();
 
 
-        } else if (id == R.id.nav_abtus) {
+        }
+        else if (id == R.id.nav_abtus) {
 
         }
 
@@ -139,11 +215,11 @@ public class Home extends AppCompatActivity
 
     public void insert(View v) {
 
-        EditText name = (EditText) findViewById(R.id.reg_name);
-        EditText email = (EditText) findViewById(R.id.reg_id);
-        EditText mobile = (EditText)findViewById(R.id.reg_mob);
-        EditText address = (EditText) findViewById(R.id.reg_add);
-        EditText password = (EditText) findViewById(R.id.reg_pass);
+        EditText name = (EditText) findViewById(id.reg_name);
+        EditText email = (EditText) findViewById(id.reg_id);
+        EditText mobile = (EditText)findViewById(id.reg_mob);
+        EditText address = (EditText) findViewById(id.reg_add);
+        EditText password = (EditText) findViewById(id.reg_pass);
         String sname = name.getText().toString();
         String semail = email.getText().toString();
         String smob = mobile.getText().toString();
@@ -189,4 +265,137 @@ public class Home extends AppCompatActivity
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
     }
+
+
+
+    // for the cards
+
+
+
+//    private void initCollapsingToolbar() {
+////        final CollapsingToolbarLayout collapsingToolbar =
+////                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+////        collapsingToolbar.setTitle(" ");
+////        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+////        appBarLayout.setExpanded(true);
+//
+//        // hiding & showing the title when toolbar expanded & collapsed
+//        //appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+//            boolean isShow = false;
+//            int scrollRange = -1;
+//
+//            @Override
+//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+//                if (scrollRange == -1) {
+//                    scrollRange = appBarLayout.getTotalScrollRange();
+//                }
+//                if (scrollRange + verticalOffset == 0) {
+//                    collapsingToolbar.setTitle(getString(R.string.app_name));
+//                    isShow = true;
+//                } else if (isShow) {
+//                    collapsingToolbar.setTitle(" ");
+//                    isShow = false;
+//                }
+//            }
+//        });
+//    }
+
+    /**
+     * Adding few albums for testing
+     */
+    private void prepareAlbums() {
+        int[] covers = new int[]{
+                R.drawable.album1,
+                R.drawable.album2,
+                R.drawable.album3,
+                R.drawable.download,
+                R.drawable.webdesign,
+                R.drawable.quiz,
+                R.drawable.treasurehunt,
+                R.drawable.itmanager,
+                R.drawable.idea,
+                R.drawable.album10,
+                R.drawable.album11};
+
+        Album a = new Album("Coding", 2, covers[0]);
+        albumList.add(a);
+
+        a = new Album("Debugging", 2, covers[1]);
+        albumList.add(a);
+
+        a = new Album("Gaming", 4, covers[2]);
+        albumList.add(a);
+
+        a = new Album("Debate", 1, covers[3]);
+        albumList.add(a);
+
+        a = new Album("Web Designing", 2, covers[4]);
+        albumList.add(a);
+
+        a = new Album("Quiz", 2, covers[5]);
+        albumList.add(a);
+
+        a = new Album("Treasure Hunt", 3, covers[6]);
+        albumList.add(a);
+
+        a = new Album("IT Manager", 1, covers[7]);
+        albumList.add(a);
+
+        a = new Album("Ideathon", 1, covers[8]);
+        albumList.add(a);
+
+        //a = new Album("Pussycat Dolls", 17, covers[9]);
+        //albumList.add(a);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+
+
 }

6d85665cf5fa593317cc6b1a92602746bcffa2d4
kanikak03 <kanikakharbanda@gmail.com>
2016-12-24 18:24:41 +0530

    Most stable

--- app/src/main/java/com/example/rajat/abhyuday/Home.java
+++ app/src/main/java/com/example/rajat/abhyuday/Home.java
@@ -1,17 +1,23 @@
 package com.example.rajat.abhyuday;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,11 +27,19 @@ import android.widget.Toast;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -41,7 +55,8 @@ public class Home extends AppCompatActivity
 
 
     SupportMapFragment smapfragment;
-
+    GoogleApiClient mGoogleApiClient;
+    GoogleMap googleMap;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -64,7 +79,6 @@ public class Home extends AppCompatActivity
         setContentView(layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(id.fab);
@@ -130,7 +144,7 @@ public class Home extends AppCompatActivity
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        android.support.v4.app.FragmentManager fm=getSupportFragmentManager();
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         android.support.v4.app.FragmentManager sfm = getSupportFragmentManager();
 
         if (smapfragment.isAdded()) {
@@ -138,7 +152,7 @@ public class Home extends AppCompatActivity
 
         }
 
-            int id = item.getItemId();
+        int id = item.getItemId();
         if (id == R.id.nav_event) {
 
             Intent mHome = new Intent(Home.this, EventActivity.class);
@@ -148,29 +162,22 @@ public class Home extends AppCompatActivity
         } else if (id == R.id.nav_schedule) {
         }
 
-//        } else if (id == R.id.nav_register) {
 //
-//
-//            RegisterFragment registerFragment=new RegisterFragment();
-//
-//            fm.beginTransaction().replace(R.id.content_home, registerFragment).commit();
-//            Toast.makeText(this, "Register", Toast.LENGTH_LONG).show();
-//
-//
-//        }
-else if (id == R.id.nav_findus) {
+        else if (id == R.id.nav_findus) {
+            if (!smapfragment.isAdded()) {
+                sfm.beginTransaction().replace(R.id.content_home, smapfragment).commit();
+
+            } else {
+                sfm.beginTransaction().show(smapfragment).commit();
+            }
 
 
-
-            sfm.beginTransaction().replace(R.id.content_home, smapfragment).commit();
-
-        }else if (id == R.id.nav_conus) {
+        } else if (id == R.id.nav_conus) {
 
             CallFragment callFragment = new CallFragment();
             fm.beginTransaction().replace(R.id.content_home, callFragment).commit();
 
-        }
-        else if (id == R.id.nav_abtus) {
+        } else if (id == R.id.nav_abtus) {
 
         }
         drawer.closeDrawer(GravityCompat.START);
@@ -181,7 +188,7 @@ else if (id == R.id.nav_findus) {
 
         EditText name = (EditText) findViewById(id.reg_name);
         EditText email = (EditText) findViewById(id.reg_id);
-        EditText mobile = (EditText)findViewById(id.reg_mob);
+        EditText mobile = (EditText) findViewById(id.reg_mob);
         EditText address = (EditText) findViewById(id.reg_add);
         EditText password = (EditText) findViewById(id.reg_pass);
         String sname = name.getText().toString();
@@ -236,16 +243,23 @@ else if (id == R.id.nav_findus) {
 
 //        LocationRequest mLocationRequest;
 //
-//        GoogleApiClient mGoogleApiClient;
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //googleMap.setMyLocationEnabled(true);
 
-        //private static final LatLng MSRIT = new LatLng(13.0311221, 77.5651647);
-        LatLng MSRIT = new LatLng(12.9718915, 77.6411545);
+        LatLng MSRIT = new LatLng(13.0311221, 77.5651647);
+        //LatLng MSRIT = new LatLng(12.9718915, 77.6411545);
 
         Marker mMsrit;
-
-
-        // Add a marker in Sydney and move the camera
 
 
         mMsrit = mMap.addMarker(new MarkerOptions()
@@ -263,6 +277,8 @@ else if (id == R.id.nav_findus) {
         mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
 
 
+
     }
+
 
 }
